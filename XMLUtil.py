e871e9dc158aeec564db3bc189886d136ace4391
Steven Swanson <swanson@cs.ucsd.edu>
2014-08-26 07:16:58 +0000

    Build/updat artwork library as on update.  Makefile for same
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/util@18531 2cf53259-273a-af40-bda6-69d31023b8ec

--- XMLUtil.py
+++ XMLUtil.py
@@ -4,5 +4,5 @@ import pipes
 def formatAndWrite(tree, file, encoding="us-ascii", xml_declaration=None, method="xml"):
     t = pipes.Template()
     t.append("xmllint --format $IN", "f-")
-    tree.write(open(file, 'w'), encoding=encoding, xml_declaration=xml_declaration, method=method)
+    tree.write(t.open(file, 'w'), encoding=encoding, xml_declaration=xml_declaration, method=method)
 
