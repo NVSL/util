431f145d9fd202641fc7e183c6a75ecbb0b0bab9
Chris Nelson <c4nelson@eng.ucsd.edu>
2015-04-30 18:41:33 +0000

    SVG artwork checker. Some of the SVGs match the package and some are dead wrong but I’m not sure why. Maybe something with the weird coordinate system
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/util@20805 2cf53259-273a-af40-bda6-69d31023b8ec

--- checkComponentArtwork.py
+++ checkComponentArtwork.py
@@ -2,21 +2,98 @@
 
 import argparse
 import SwoopGeom
+import Swoop
 from lxml import etree as ET
 import os
 import sys
 import glob
 from os.path import join
 from Dingo.Rectangle import Rectangle
+import svgwrite
 import numpy as np
+import re
 
 def missing_tag(name, bad_tag, missing_attr):
     sys.stderr.write("Error in component {0}: <{1}> is missing attribute {2}\n".
                      format(name, bad_tag.tag, missing_attr))
 
 
-def check_artwork_against_package(svg_path, swoop_package):
-    pass
+def check_artwork_against_package(svg_path, swoop_from_package):
+    def close(val1, val2, atol = 0.5):
+         # Custom compare with tolerance
+        return abs(val1 - val2) <= atol
+
+    assert isinstance(swoop_from_package, Swoop.From)
+    assert len(swoop_from_package)==1
+    assert isinstance(swoop_from_package[0], Swoop.Package)
+    boxes = package.get_children().get_bounding_box()
+    if len(boxes)==0:
+        sys.stderr.write("Package {0} has no bounding box\n".format(swoop_from_package[0].get_name()))
+        return False
+    rect = boxes.reduce(Rectangle.union)
+    svg_name = os.path.basename(svg_path)
+    try:
+        svg = ET.parse(svg_path).getroot()
+    except ET.XMLSyntaxError:
+        sys.stderr.write("Invalid SVG: {0}\n".format(svg_path))
+        return False
+    mm_dim = re.compile(r"(\d+(\.\d+)?)mm")
+    height =  float(re.match(mm_dim, svg.get("height")).group(1))
+    width = float(re.match(mm_dim, svg.get("width")).group(1))
+    if not close(width, rect.width, 2):
+        sys.stderr.write("{0} has width {1}mm, but the package has width {2}mm\n".
+                         format(svg_name, width, rect.width))
+        return False
+    if not close(height, rect.height, 2):
+        sys.stderr.write("{0} has height {1}mm, but the package has height {2}mm\n".
+                         format(svg_name, height, rect.height))
+        return False
+
+    bottom_left = np.array(map(float, svg.get("viewBox").split()[:2]))
+    if not np.allclose(bottom_left, rect.bounds[0]):
+        sys.stderr.write("{0} has its lower-left at {1}, the package has it at {2}\n".\
+            format(svg_name, bottom_left, rect.bounds[0]))
+        return False
+
+    return True
+
+
+
+
+def get_package(all_libraries, libname, device_name, variant):
+    lib = libraries.get(libname)
+    if lib is None:
+        sys.stderr.write("Nonexistent library {0}\n".
+                         format(libname))
+        return None
+    deviceset = lib.get_deviceset(device_name)
+    if variant is None:
+        sys.stderr.write("Missing variant\n")
+        return None
+    return deviceset.get_device(variant).find_package()
+
+class LazyItem(object):
+    def __init__(self, path):
+        self.path = path
+        self.library = None
+
+# Only Swoop (swipe?) libraries when you need them
+# People may have a ton of Eagle libraries
+class LibraryCollectionLazy(object):
+    def __init__(self, libs_dir):
+        self._libraries = {}
+        for lib in glob.glob(libs_dir + "/*.lbr"):
+            basename = os.path.basename(lib)
+            self._libraries[os.path.splitext(basename)[0]] = LazyItem(lib)
+
+    def get(self, libname):
+        path_lib = self._libraries.get(libname)
+        if path_lib is None:
+            return None
+        if path_lib.library is None:
+            print "loading " + libname
+            path_lib.library = SwoopGeom.from_file(path_lib.path).get_library()
+        return path_lib.library
 
 
 parser = argparse.ArgumentParser(description="Checks that the component artwork viewbox"
@@ -33,11 +110,8 @@ lib_dir = os.environ.get("EAGLE_LIBS") or sys.exit("Did you source setup_gadgets
 components_dir = os.environ.get("GADGETRON_COMPONENT_LIB")
 catalog_dir = os.path.dirname(args.catalog)
 
-print "Loading libraries..."
-libraries = {}
-for lib in glob.glob(lib_dir + "/*.lbr"):
-    basename = os.path.basename(lib)
-    libraries[os.path.splitext(basename)[0]] = SwoopGeom.from_file(lib).get_library()
+
+libraries = LibraryCollectionLazy(lib_dir)
 
 ARE_WE_GOOD = True
 
@@ -56,6 +130,8 @@ for component in xml.findall("component"):
         continue
 
     if device is not None and len(placed_parts)==1:
+        # Check from the <eagledevice> tag
+        # Only one artwork file means we know which it is
         device_name = device.get("device-name")
         if device_name is None:
             missing_tag(keyname, device, "device-name")
@@ -63,20 +139,8 @@ for component in xml.findall("component"):
         libname = device.get("library")
         if libname=="NONE":
             continue
-        lib = libraries.get(libname)
-        if lib is None:
-            sys.stderr.write("{0} specified nonexistent library {1} in <{2}>\n".
-                             format(keyname, libname, device.tag))
-            ARE_WE_GOOD = False
-            continue
-        deviceset = lib.get_deviceset(device.get("device-name"))
         variant = device.get("variant")
-        if variant is None:
-            sys.stderr.write("{0} does not specify a variant in <{1}>\n".
-                             format(keyname, device.tag))
-            ARE_WE_GOOD=False
-            continue
-        package = deviceset.get_device(variant).find_package()
+        package = get_package(libraries, libname, device_name, variant)
         if len(package)==0:
             sys.stderr.write("Could not find the package for {0} from its <{1}> tag\n".
                              format(keyname, device.tag))
@@ -90,9 +154,11 @@ for component in xml.findall("component"):
             continue
 
         artwork_svg_file = join(catalog_dir, svg)
+        check_artwork_against_package(artwork_svg_file, package)
 
-    #Pull the package from the schematic
-    # sys.exit("{0} has no <eagledevice> defined".format(component.get("keyname")))
+
+    # Pull the package from the schematic
+    # Either there is no <eagledevice> or there are multiple artwork files
     if component.find("schematic") is not None:
         dir = join(components_dir, "Catalog", component.find("homedirectory").text)
         schematic_file = join(dir, component.find("schematic").get("filename"))
@@ -101,23 +167,28 @@ for component in xml.findall("component"):
         schematic = SwoopGeom.from_file(schematic_file)
 
         for placedpart in placed_parts:
-            part = schematic.get_part(placedpart.get("refdes"))
-            if len(part)==0:
-                sys.stderr.write("refdes {0} of {1} refers to a nonexistent schematic part {2}\n".
-                                 format(placedpart.get("refdes"),keyname,placedpart.get("model2D")))
+            refdes = placedpart.get("refdes")
+            if refdes is None:
+                missing_tag(keyname, placedpart, "refdes")
                 ARE_WE_GOOD=False
                 continue
-    continue
-
-
-    boxes = package.get_children().get_bounding_box()
-    if len(boxes)==0:
-        print package.get_children()
-        sys.stderr.write("{0} has a package but no bounding box\n".format(keyname))
-        ARE_WE_GOOD = False
-        continue
-
-    rect = boxes.reduce(Rectangle.union)
+            part = schematic.get_part(refdes)
+            if len(part)==0:
+                sys.stderr.write("refdes '{0}' of {1} refers to a nonexistent schematic part. "
+                                 "Schematic file: {2}\n".
+                                 format(placedpart.get("refdes"),keyname,schematic_file))
+                ARE_WE_GOOD=False
+                continue
+            package = part.find_package()
+            if len(package)==0:
+                #TODO: probably a koala part
+                # sys.stderr.write("Part {0} in {1} has no package\n".format(refdes, schematic_file))
+                continue
+            artwork_svg_file = join(catalog_dir, placedpart.get("model2D"))
+            check_artwork_against_package(artwork_svg_file, package)
 
 if not ARE_WE_GOOD:
     sys.exit("Some components have errors")
+else:
+    print "Success!"
+
