339930168d35dece28e801904703b7c1da7a81d1
Chris Nelson <c4nelson@eng.ucsd.edu>
2015-06-01 18:32:40 +0000

    Permissions
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/util@21304 2cf53259-273a-af40-bda6-69d31023b8ec

--- casemaker.py
+++ casemaker.py
@@ -19,8 +19,24 @@ def make_cutout_rect(container, rect, thickness):
     """
     If rect intersects container, return a rectangle to cut out the container
     """
-    pass
+    if container.encloses(rect):
+        return None
 
+    cut = rect
+    for axis in xrange(2):
+        if rect.low(axis) < container.low(axis):    #left/bottom cut
+            move = np.array([0,0])
+            move[axis] = -thickness*2
+            if cut is not None:
+            cut = Rectangle.union(cut, rect.copy().move(move))
+            cut = rect.copy().move(move)
+            # print cut
+        if rect.high(axis) > container.high(axis):  #right/top cut
+            move = np.array([0,0])
+            move[axis] = thickness*2
+            cut = Rectangle.union(cut, rect.copy().move(move))
+            # print cut
+    return cut
 
 parser = argparse.ArgumentParser(description="Make 3D printed cases for your gadgets")
 parser.add_argument("brdfile", help="Board file to make a case from")
@@ -34,14 +50,14 @@ board_box = board.get_bounding_box()
 
 
 SPACE = 15.0    #space between board and top/bottom of case
-THICKNESS = 6   #thickness of plastic around case
+thickness = 6   #thickness of plastic around case
 
 #The shell, which will be hollowed out
 inner = translate([board_box.left(), board_box.bot(), -SPACE])(
             cube([board_box.width, board_box.height, SPACE*2])
 )
 
-outer_shell = minkowski()(inner, sphere(THICKNESS))
+outer_shell = minkowski()(inner, sphere(thickness))
 
 case = outer_shell - inner
 
@@ -58,34 +74,16 @@ tfaceplate += board.get_plain_elements().filtered_by(tfaceplate_filter).get_boun
 
 for rect in tfaceplate:
     case -= rect2scad(rect,SPACE + 30) #top
-    padded = rect.copy().pad(1.0)
-    if not board_box.encloses(padded):  #faceplate sticks out the side
+    container = rect.copy().pad(1.0)
+    if not board_box.encloses(container):  #faceplate sticks out the side
         # print "side cut {0}".format(rect.eagle_code())
-        cut = None
-        for axis in xrange(2):
-            if padded.low(axis) < board_box.low(axis):    #left/bottom cut
-                move = np.array([0,0])
-                move[axis] = -THICKNESS*2
-                if cut is not None:
-                    cut = Rectangle.union(cut, rect.copy().move(move))
-                else:
-                    cut = rect.copy().move(move)
-                # print cut
-            if padded.high(axis) > board_box.high(axis):  #right/top cut
-                move = np.array([0,0])
-                move[axis] = THICKNESS*2
-                if cut is not None:
-                    cut = Rectangle.union(cut, rect.copy().move(move))
-                else:
-                    cut = rect.copy().move(move)
-                # print cut
+        cut = make_cutout_rect(board, container, thickness)
         if cut is not None:
             case -= rect2scad(cut, SPACE)
 
 side_cuts = board.get_plain_elements().\
     filtered_by(lambda p: hasattr(p,"layer") and p.layer=="sideCut").\
     get_bounding_box()
-
 
 
 if args.file:
