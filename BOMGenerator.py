ef459327104c935b2195a5b63b6dc712b8edc3fa
Michael Gonzalez <mmg005@eng.ucsd.edu>
2015-10-12 04:49:28 +0000

    BOM script now reads price info from components catalog
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/util@22615 2cf53259-273a-af40-bda6-69d31023b8ec

--- BOMGenerator.py
+++ BOMGenerator.py
@@ -3,8 +3,21 @@
 # BOMGenerator script
 import Swoop
 import argparse
+import xml.etree.ElementTree as ET
+
+def parseSchematic( schematic ):
+     for n in (Swoop.From(schematic).get_parts()):
+         print n.get_name() 
 
 parser = argparse.ArgumentParser(description="Generates a Bill of Materials from a schematic file")
 parser.add_argument("-i", "--insch", required=True)
 args = parser.parse_args()
 schematic = Swoop.from_file(args.insch )
+parseSchematic( schematic )
+tree = ET.parse('../../Libraries/Components/Catalog/Catalog/Components.cat')
+root = tree.getroot()
+for child in root:
+    elem = child.find("supplier")
+    if( elem != None ):
+       price = elem.attrib["price"]
+       print child.attrib["keyname"], "price: ", price
