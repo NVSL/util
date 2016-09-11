ca06c406e577ba52532af96b7e1703f1cdc42487
Steven Swanson <swanson@cs.ucsd.edu>
2016-09-11 13:56:15 -0700

    get rid of 'print'

--- gtron_util/pyUtil.py
+++ gtron_util/pyUtil.py
@@ -1,7 +1,7 @@
 import subprocess
 
-def docmd(cmd, simulate=False):
+def docmd(cmd, simulate=False, stderr=None, stdout=None, stdin=None):
     if simulate:
         print cmd
     else:
-        subprocess.call(cmd, shell=True)       
+        subprocess.call(cmd, shell=True, stdout=stdout, stderr=stderr, stdin=stdin)
